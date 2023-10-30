f1b4517e6575301d06df594c7159fa750a96647d
PetrSin <Petrsinisyn2015@gmail.com>
2023-10-30 18:54:54 +0300

    Отправка POST запросов и отображение их в таблице курсов

---
+++ TabelView+Networking/ViewControllers/PostCourseAlamofierViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostCourseAlamofierViewController.swift
+//  TabelView+Networking
+//
+//  Created by petar on 30.10.2023.
+//
+
+import UIKit
+
+class PostCourseAlamofierViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
